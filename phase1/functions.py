a202038b712a82ef6ae4b6e0c17c290916d0f34f
gwenmunson <gwen.munson15@gmail.com>
2019-10-06 12:08:03 -0400

    Ignored idea folder, fixed spelling mistake + added colons to functions

--- phase1/functions.py
+++ phase1/functions.py
@@ -1,29 +1,29 @@
 # functions to be called when interpreting from decorated tree
 
-def add(a,b)
+def add(a,b):
     return a + b
 
-def subtract(a,b)
+def subtract(a,b):
     return a - b
 
-def divideInt(a,b)
+def divideInt(a,b):
     return a // b
 
-def devideDbl(a,b)
+def divideDbl(a,b):
     return a / b
 
-def multiply(a,b)
+def multiply(a,b):
     return a * b
 
-def power(a,b)
+def power(a,b):
     return a ** b
 
-def print(strOrNum)
+def print(strOrNum):
     print(strOrNum)
     return
 
-def concat(str1,str2)
+def concat(str1,str2):
     return str1 + str2
 
-def charAt(str,index)
+def charAt(str,index):
     return str[index]
